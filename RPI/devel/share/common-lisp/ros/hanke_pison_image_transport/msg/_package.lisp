efbdb74b1b220d8204499f95c56ce0273a1a2532
hanke-pison <35685908+hanke-pison@users.noreply.github.com>
2018-05-28 09:09:47 +0800

    Add RPI/devel files via upload

---
+++ RPI/devel/share/common-lisp/ros/hanke_pison_image_transport/msg/_package.lisp
@@ -0,0 +1,7 @@
+(cl:defpackage hanke_pison_image_transport-msg
+  (:use )
+  (:export
+   "<RESIZEDIMAGE>"
+   "RESIZEDIMAGE"
+  ))
+
---
+++ RPI/devel/share/hanke_pison_image_transport/cmake/hanke_pison_image_transport-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(hanke_pison_image_transport_MESSAGE_FILES "/home/hanke/RPI/src/hanke_pison_image_transport/msg/ResizedImage.msg")
+set(hanke_pison_image_transport_SERVICE_FILES "")
