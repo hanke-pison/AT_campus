2b81ca5a0939e36c931a46a4ea8df40c83e85605
hanke-pison <35685908+hanke-pison@users.noreply.github.com>
2018-05-28 09:15:08 +0800

    Add RPI/build files via upload

---
+++ RPI/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/hanke/RPI/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/hanke/RPI/devel/lib:$LD_LIBRARY_PATH"
+export PATH="/opt/ros/lunar/bin:/home/hanke/bin:/home/hanke/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PKG_CONFIG_PATH="/home/hanke/RPI/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/hanke/RPI/build"
+export PYTHONPATH="/home/hanke/RPI/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/hanke/RPI/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/pike/RPI_NO1/src:/home/hanke/RPI_NO1/src:/home/pike/RPI/src:/home/hanke/RPI/src:$ROS_PACKAGE_PATH"
